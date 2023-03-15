df78794455f78eb73e97a17d5f5ec2cd4c654441
SIR-97 <sirafiq.1997@gmail.com>
2023-03-15 23:08:15 +0000

    created variables that will apply to the elements taken from the html files

---
+++ starter/assets/js/logic.js
@@ -0,0 +1,11 @@
+// Get necessary elements from HTML
+var startButton = document.getElementById("start");
+var timeEl = document.getElementById("time");
+var questionTitleEl = document.getElementById("question-title");
+var choicesEl = document.getElementById("choices");
+var feedbackEl = document.getElementById("feedback");
+var initialsEl = document.getElementById("initials");
+var submitButton = document.getElementById("submit");
+var finalScoreEl = document.getElementById("final-score");
+
+ 
