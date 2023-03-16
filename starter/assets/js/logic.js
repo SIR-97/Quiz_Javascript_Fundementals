59a58e4c79f78c8eebe546098351c1ecebcac9ae
SIR-97 <sirafiq.1997@gmail.com>
2023-03-16 00:01:18 +0000

    added my quiz questions with choices and defined the correct answer

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -8,4 +8,32 @@ var initialsEl = document.getElementById("initials");
 var submitButton = document.getElementById("submit");
 var finalScoreEl = document.getElementById("final-score");
 
- 
+
+// Set up quiz questions
+var questions = [
+  {
+  title: "What does JS stand for?",
+  choices: ["Java Style", "JavaScript", "Just Saying"],
+  answer: "JavaScript"
+},
+{
+  title: "Which of the following is NOT a valid primitive data type in JavaScript?",
+  choices: ["string", "number", "array"],
+  answer: "array"
+},
+{
+  title: "What is the difference between == and === in JavaScript?",
+  choices: ["== performs type coercion while === does not", "== and === perform the same operation", "== is used for comparing numbers while === is used for comparing strings"],
+  answer: "== performs type coercion while === does not"
+},
+{ title: "What is the purpose of the 'if' statement in JavaScript?",    
+  choices: ["To declare a variable", "To define a function", "To control the flow of execution based on a condition"],
+  answer: "To control the flow of execution based on a condition"
+},
+{
+  title: "What is the purpose of the 'for' loop in JavaScript?",
+  choices: ["To declare a function", "To control the flow of execution based on a condition", "To execute a block of code repeatedly"],
+  answer: "To execute a block of code repeatedly"
+},
+];
+
